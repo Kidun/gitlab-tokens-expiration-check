e41ab315f36ddcaf72ecf2ab2a5cc433b2e86cc8
Ruslan Kidun <ruslan.kidun@gmail.com>
2023-09-19 09:37:27 +0300

    Quick and dirty

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # gitlab-tokens-expiration-check
 Gitlab simple tool which grabs all groups' and projects' tokens and print them with expiration dates 
+
+##Install
+Download the code
+
+```go build
+```
+##Usage:
+```%program_name% token url
+```
---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/xanzy/go-gitlab"
+)
+
+func gitlabConnect(pat string, url string) (*gitlab.Client, error) {
+	// Initialize the GitLab API client with your access token
+	client, err := gitlab.NewClient(pat, gitlab.WithBaseURL(url))
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	return client, err
+}
+
+func main() {
+	const defToken = ""
+	const defURL = "https://gitlab.com"
+
+	listOptions := gitlab.ListOptions{
+		PerPage: 100,
+		Page:    1,
+	}
+	gitlabToken := defToken
+	url := defURL
+
+	if len(os.Args) > 2 {
+		gitlabToken = os.Args[1]
+		url = os.Args[2]
+	} else {
+		fmt.Printf("Usage %s token url\n", os.Args[0])
+		return
+	}
+
+	gitlabClient, err := gitlabConnect(gitlabToken, url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Projects' tokens
+	for {
+		projects, resp, err := gitlabClient.Projects.ListProjects(&gitlab.ListProjectsOptions{ListOptions: listOptions})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, prj := range projects {
+			tokens, _, err := gitlabClient.ProjectAccessTokens.ListProjectAccessTokens(prj.ID, &gitlab.ListProjectAccessTokensOptions{Page: 1, PerPage: 20})
+			for _, token := range tokens {
+				log.Printf("Project #%d (%s): Token #%d (%s) expires %s", prj.ID, prj.Name, token.ID, token.Name, token.ExpiresAt.String())
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+		}
+
+		if resp.CurrentPage >= resp.TotalPages {
+			//resp.TotalPages {
+			break
+		}
+		listOptions.Page = resp.NextPage
+	}
+
+	// Groups' tokens
+	listOptions.Page = 1
+	for {
+		groups, resp, err := gitlabClient.Groups.ListGroups(&gitlab.ListGroupsOptions{ListOptions: listOptions})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, group := range groups {
+			tokens, _, err := gitlabClient.GroupAccessTokens.ListGroupAccessTokens(group.ID, &gitlab.ListGroupAccessTokensOptions{Page: 1, PerPage: 20})
+			for _, token := range tokens {
+				log.Printf("Group #%d (%s): Token #%d (%s) expires %s", group.ID, group.Name, token.ID, token.Name, token.ExpiresAt.String())
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+		}
+
+		if resp.CurrentPage >= resp.TotalPages {
+			//resp.TotalPages {
+			break
+		}
+		listOptions.Page = resp.NextPage
+	}
+}
